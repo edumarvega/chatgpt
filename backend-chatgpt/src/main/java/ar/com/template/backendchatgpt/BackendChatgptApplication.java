b848d0833fa7d3ddecf2336ba4e31d039a906c62
Martin Vega <martin.vega@osde.com.ar>
2023-04-24 17:38:47 -0300

    se agrega paddin al body

---
+++ backend-chatgpt/src/main/java/ar/com/template/backendchatgpt/BackendChatgptApplication.java
@@ -0,0 +1,13 @@
+package ar.com.template.backendchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChatgptApplication.class, args);
+	}
+
+}
---
+++ backend-chatgpt/src/test/java/ar/com/template/backendchatgpt/BackendChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.template.backendchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
